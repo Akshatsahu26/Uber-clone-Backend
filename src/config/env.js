87b97870430baf68ec027babb7707d370b4b8c04
Akshat Sahu <sahuakshat2611@gmail.com>
2026-03-14 12:22:38 +0530

    fixed

--- src/config/env.js
+++ src/config/env.js
@@ -1,28 +1,33 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
-const requiredEnvVars = ['PORT', 'AUTHOR_NAME',"MONGODB_URL","JWT_SECRET","JWT_EXPIRES_IN" ];
-requiredEnvVars.forEach((envVar)=>{
-    if(!process.env[envVar]){
-        throw new Error(`Missing required environment variable: ${envVar}`);
-    }
-})
+const requiredEnvVars = [
+  "MONGODB_URL",
+  "JWT_SECRET",
+  "JWT_EXPIRES_IN"
+];
+
+requiredEnvVars.forEach((envVar) => {
+  if (!process.env[envVar]) {
+    throw new Error(`Missing required environment variable: ${envVar}`);
+  }
+});
 
 export const env = {
-    PORT: process.env.PORT,
-    AUTHOR_NAME: process.env.AUTHOR_NAME,
-    MONGODB_URL: process.env.MONGODB_URL,
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
-    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
+  PORT: process.env.PORT || 8000,
+  AUTHOR_NAME: process.env.AUTHOR_NAME || "Akshat",
+  MONGODB_URL: process.env.MONGODB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 
-    // Kafka Configuration
-    KAFKA_ENABLED: process.env.KAFKA_ENABLED === 'true',
-    KAFKA_BROKER: process.env.KAFKA_BROKER || 'localhost:9092',
-    KAFKA_USERNAME: process.env.KAFKA_USERNAME || '',
-    KAFKA_PASSWORD: process.env.KAFKA_PASSWORD || '',
-    KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID || 'uber-clone-backend',
-   
+  ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
+
+  KAFKA_ENABLED: process.env.KAFKA_ENABLED === "true",
+  KAFKA_BROKER: process.env.KAFKA_BROKER || "localhost:9092",
+  KAFKA_USERNAME: process.env.KAFKA_USERNAME || "",
+  KAFKA_PASSWORD: process.env.KAFKA_PASSWORD || "",
+  KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID || "uber-clone-backend",
 };
-
